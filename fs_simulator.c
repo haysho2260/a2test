314ba2d128c2e4db5728eb1f85a80ed4f9af3308
haysho2260 <haysho2260@gmail.com>
2023-04-25 03:15:04 +0000

    req 1, 2

--- fs_simulator.c
+++ fs_simulator.c
@@ -3,26 +3,67 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdint.h>
 
+int find_dir(char* fileDir);
 
 int main(int argc, char *argv[]) {
-    if (chdir(argv[1]) != -1){
-        printf("Success reading directory!\n");
-    }
-    else {
-        perror("chdir failed");
-        printf("errno = %d\n", errno);
-    }
+    find_dir(argv[1]);
+    chdir("./empty");
     FILE *fp = fopen("inodes_list", "rb");
     if (fp == NULL) {
         perror("Failed to open file");
         return 1;
     }
-    int byte;
-    while ((byte = fgetc(fp)) != EOF) {
-        printf("%02x\n", byte);
+    uint32_t num;
+    char ch;
+    char inodes[1024];
+    while (fread(&num, sizeof(num), 1, fp) == 1 &&
+            fread(&ch, sizeof(ch), 1, fp) == 1){
+        if ((0 <= num) && (num <= 1023)){
+            inodes[num] = ch;
+        } else{
+            printf("Inode: %d is invalid\n", num);
+        }
     }
     fclose(fp);
     return 0;
 
+}
+
+int find_dir(char* fileDir){
+    if (access(fileDir, F_OK) == 0){
+        printf("Successfully found %s\n", fileDir);
+    }
+    else {
+        perror("Error: ");
+        printf("errno = %d\n", errno);
+    }
+    return 0;
+}
+
+
+int init_sim(){
+    chdir("./empty");
+    FILE *fp = fopen("inodes_list", "rb");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+    uint32_t num;
+    char ch;
+    while (fread(&num, sizeof(num), 1, fp) == 1 &&
+            fread(&ch, sizeof(ch), 1, fp) == 1){
+        if ((num < 0) || (num > 1023)){
+                printf("Inode: %d is invalid\n", num);
+        }
+        // char inode[1024];
+        // if ((0 <= num) && (num <= 1023)){
+            //inodes[num] = ch;
+        // } else{
+        //    printf("Inode: %d is invalid\n", num);
+        // }
+    }
+    fclose(fp);
+    return 0;
 }
